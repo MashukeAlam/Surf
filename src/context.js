6500bd40b7b67cef3760bee97c74f9c90adaa8b9
MashukJim <jimmashuke@gmail.com>
2020-11-11 20:55:05 +0600

    Cleanign Stuffs

--- src/context.js
+++
@@ -1,5 +0,0 @@
-// console.log('Alhamdulillah');
-// document.addEventListener('contextmenu', function(e) {
-//     alert(e.target.src || e.target.href); //here you draw your own menu
-//     e.preventDefault();
-//   }, false);
--- src/index.js
+++ src/index.js
@@ -10,38 +10,23 @@ const {
 } = require('electron')
 const url = require('url');
 const path = require('path');
-// const contextMenu = require('electron-context-menu')
 
-
-
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
-//now
-
 let win;
 
+// //IPC
+// ipcMain.on('test', (e, args) => {
+//   if (args !== null) {
+//     console.log(args);
+//   }
 
-// const menu = new Menu();
-// menu.append(new MenuItem({ label: 'MenuItem1', click() { console.log('item 1 clicked') } }))
-// menu.append(new MenuItem({ type: 'separator' }))
-// menu.append(new MenuItem({ label: 'MenuItem2', type: 'checkbox', checked: true }))
-
-//IPC
-ipcMain.on('test', (e, args) => {
-  if (args !== null) {
-    console.log(args);
-  }
-
-  e.sender.send('test', 'Received!');
-});
-
-
+//   e.sender.send('test', 'Received!');
+// });
 
 const createWindow = () => {
-  //Browser windows config
   const _width = screen.getPrimaryDisplay().bounds.width
   const _height = screen.getPrimaryDisplay().bounds.height
 
@@ -70,19 +55,11 @@ const createWindow = () => {
   win.webContents.on('context-menu', (e, params) => {
     contextMenu();
   })
-
-
-
-
 };
-
 
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
@@ -93,50 +70,3 @@ app.on('activate', () => {
     createWindow();
   }
 });
-
-// app.on("web-contents-created", (e, contents) => {
-//   // contextMenu({
-//   //    window: contents,
-//   //    showSearchWithGoogle: true,
-//   //    showSaveImageAs: true,
-//   //    showInspectElement: true
-//   // });
-//   // const contextMenu = require('electron-context-menu');
-
-//   // This code adds 2 new items to the context menu to zoom in the window (in and out)
-//   // Read other steps for more information
-
-
-// // });
-// app.on("web-contents-created", (e, contents) => { if (contents.getType() == "webview") { 
-//   contextMenu({
-//     window: webview,
-//     showSaveImageAs: true,
-//     showSearchWithGoogle: true,
-//     showInspectElement: true,
-//     showSaveLinkAs: true,
-//     prepend: (params, actions, browserWindow) => [
-//       {
-//         role: "zoomIn"
-//       },
-//       {
-//         role: "zoomOut"
-//       },
-//       {
-//         label: 'On new tab',
-        
-//         click: () => {
-//           // console.log(params.linkUrl, params.mediaType);
-//           // console.log(actions);
-//           // const everything_down_to_one = JSON.parse(JSON.stringify(actions))['linkURL'];
-//           // console.log(JSON.parse(everything)['linkURL']);
-//           // console.log(everything["linkURL"]);
-//           // console.log(everything.linkURL);
-//           // createTabs(everything_down_to_one, processTabs);
-
-//         }
-//       }
-//     ],
-//   });
-  
-//  } });
